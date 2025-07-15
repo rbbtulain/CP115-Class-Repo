9f11547c6e65bf44de4876c17e5745be7e84fe0f
rbbtulain <rbbtulain@gmail.com>
2025-07-15 12:29:59 +0800

    update exercise.py

--- labs/lab02/tests/test_lab02.py
+++ labs/lab02/tests/test_lab02.py
@@ -3,4 +3,4 @@ import sys
 
 def test_lab02_prints_hello():
     output = subprocess.check_output([sys.executable, "labs/lab02/exercise.py"])
-    assert b"Hello, Lab 02" in output
+    assert b"Hello, Lab 02 " in output
