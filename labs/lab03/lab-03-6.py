641d929b8511c5be9b37dcc5579ad61033c0b3b2
rbbtulain <rbbtulain@gmail.com>
2025-07-28 21:43:50 +0800

    update exercise.py

--- labs/lab03/lab-03-6.py
+++ labs/lab03/lab-03-6.py
@@ -2,5 +2,5 @@ yardLength = float(input())
 yardWidth = float(input())
 houseLength = float(input())
 houseWidth = float(input())
-WAGE = yardLength * yardWidth - houseLength * houseWidth * 2.0
+WAGE = (yardLength * yardWidth - houseLength * houseWidth) * 2.0
 print(WAGE)
