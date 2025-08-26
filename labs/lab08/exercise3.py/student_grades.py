ce8c2a8ce518cfe1d64b2b7618e3eae659897420
rbbtulain <rbbtulain@gmail.com>
2025-08-26 12:44:18 +0800

    exercise

--- labs/lab08/exercise3.py/student_grades.py
+++ labs/lab08/exercise3.py/student_grades.py
@@ -2,10 +2,10 @@ test1 = 78
 test2 = 85
 test3 = 92
 test4 = 67
-tset5 = 88
+test5 = 88
 
-total_points = test1 + test2 + test3 + test4
-average_score = (test1 + test2 + test3 + test4) / 4
+total_points = test1 + test2 + test3 + test4 + test5
+average_score = (test1 + test2 + test3 + test4 + test5) / 5
 
 print(f"Average Score: {average_score} (Type: {float(average_score)})")
 print(f"Total Points: {total_points} (Type: {int(total_points)})")
