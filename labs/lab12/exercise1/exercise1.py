1f2559bb5dfc48fdd3a690a6a96fef0dffcc333d
rbbtulain <rbbtulain@gmail.com>
2025-10-07 12:49:30 +0800

    husjdn

--- labs/lab12/exercise1/exercise1.py
+++ labs/lab12/exercise1/exercise1.py
@@ -3,7 +3,7 @@ price = float(input())
 item_count = 0
 total_cost = 0.0
 
-while price != -1:
+while price != -1 and price >= 0:  # Condition
     item_count += 1
     total_cost += price
     price = float(input())
--- labs/lab12/exercise2/exercise2.py
+++ labs/lab12/exercise2/exercise2.py
@@ -1,13 +1,16 @@
 score = float(input())
 
-average_score = 0.0
 score_count = 0
 total_score = 0.0
+average_score = 0.0 
 
-while score > 0 and score < 100:  # Condition
+while 0 <= score <= 100:
     score_count += 1
     total_score += score
-    score = float(input())
+    score = float(input()) 
+
+if score_count > 0:
+    average_score = total_score / score_count
 
 print(score_count)
 print(total_score)
